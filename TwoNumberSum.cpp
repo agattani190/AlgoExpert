f57aaed1e619e67c995d7fddc264849d96cabf25
Akshay Gattani <agattani@Akshays-MacBook-Pro.local>
2021-08-23 19:54:15 -0400

    Two Number Sum C++ Solution

---
+++ TwoNumberSum.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+vector<int> twoNumberSum(vector<int> array, int targetSum)
+{
+    sort(array.begin(), array.end());
+    int startPtr = 0;
+    int endPtr = array.size() - 1;
+    while (startPtr < endPtr)
+    {
+        if (array[startPtr] + array[endPtr] == targetSum)
+        {
+            return {array[startPtr], array[endPtr]};
+        }
+        else if (array[startPtr] + array[endPtr] < targetSum)
+        {
+            startPtr++;
+        }
+        else
+        {
+            endPtr--;
+        }
+    }
+    return {};
+}
