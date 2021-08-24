18443287465a0ba29571bedd675b18308c6100b8
Akshay Gattani <agattani@vt.edu>
2021-08-24 16:51:24 -0400

    Merge Overlapping Intervals AlgoExpert

--- AlgoExpert/SortedSquaredArray2.cpp
+++ AlgoExpert/SortedSquaredArray2.cpp
@@ -1,39 +1,33 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 // Time Complexity: O(n)
 // Space Complexity: O(n)
-vector<int> sortedSquaredArray(vector<int> array)
-{
-    int arraySize = array.size();
-    vector<int> sortedSquaredArray(arraySize);
-    int indexOfFirstNonNegativeNumber = lower_bound(array.begin(), array.end(), 0) - array.begin();
-    int rightPtr = indexOfFirstNonNegativeNumber;
-    int leftPtr = rightPtr - 1;
-    int index = 0;
-    while (leftPtr >= 0 && rightPtr < arraySize)
-    {
-        if (abs(array[leftPtr]) <= abs(array[rightPtr]))
-        {
-            sortedSquaredArray[index++] = array[leftPtr] * array[leftPtr];
-            leftPtr--;
-        }
-        else
-        {
-            sortedSquaredArray[index++] = array[rightPtr] * array[rightPtr];
-            rightPtr++;
-        }
+vector<int> sortedSquaredArray(vector<int> array) {
+  int arraySize = array.size();
+  vector<int> sortedSquaredArray(arraySize);
+  int indexOfFirstNonNegativeNumber =
+      lower_bound(array.begin(), array.end(), 0) - array.begin();
+  int rightPtr = indexOfFirstNonNegativeNumber;
+  int leftPtr = rightPtr - 1;
+  int index = 0;
+  while (leftPtr >= 0 && rightPtr < arraySize) {
+    if (abs(array[leftPtr]) <= abs(array[rightPtr])) {
+      sortedSquaredArray[index++] = array[leftPtr] * array[leftPtr];
+      leftPtr--;
+    } else {
+      sortedSquaredArray[index++] = array[rightPtr] * array[rightPtr];
+      rightPtr++;
     }
-    while (leftPtr >= 0)
-    {
-        sortedSquaredArray[index++] = array[leftPtr] * array[leftPtr];
-        leftPtr--;
-    }
-    while (rightPtr < arraySize)
-    {
-        sortedSquaredArray[index++] = array[rightPtr] * array[rightPtr];
-        rightPtr++;
-    }
-    return sortedSquaredArray;
+  }
+  while (leftPtr >= 0) {
+    sortedSquaredArray[index++] = array[leftPtr] * array[leftPtr];
+    leftPtr--;
+  }
+  while (rightPtr < arraySize) {
+    sortedSquaredArray[index++] = array[rightPtr] * array[rightPtr];
+    rightPtr++;
+  }
+  return sortedSquaredArray;
 }
--- AlgoExpert/TwoNumberSum.cpp
+++ AlgoExpert/TwoNumberSum.cpp
@@ -3,25 +3,18 @@ using namespace std;
 
 // Time Complexity: O(nlogn)
 // Space Complexity: O(n)
-vector<int> twoNumberSum(vector<int> array, int targetSum)
-{
-    sort(array.begin(), array.end());
-    int startPtr = 0;
-    int endPtr = array.size() - 1;
-    while (startPtr < endPtr)
-    {
-        if (array[startPtr] + array[endPtr] == targetSum)
-        {
-            return {array[startPtr], array[endPtr]};
-        }
-        else if (array[startPtr] + array[endPtr] < targetSum)
-        {
-            startPtr++;
-        }
-        else
-        {
-            endPtr--;
-        }
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+  sort(array.begin(), array.end());
+  int startPtr = 0;
+  int endPtr = array.size() - 1;
+  while (startPtr < endPtr) {
+    if (array[startPtr] + array[endPtr] == targetSum) {
+      return {array[startPtr], array[endPtr]};
+    } else if (array[startPtr] + array[endPtr] < targetSum) {
+      startPtr++;
+    } else {
+      endPtr--;
     }
-    return {};
+  }
+  return {};
 }
