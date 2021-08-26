268f1f34999302494df13b25827b70b2ae2f2791
Akshay Gattani <agattani@vt.edu>
2021-08-26 19:13:11 -0400

    Cycle in Graph AlgoExpert

--- AlgoExpert/CycleInGraph.cpp
+++ AlgoExpert/CycleInGraph.cpp
@@ -4,24 +4,29 @@ using namespace std;
 // Time Complexity: O(V + E) -> E is number of edges and V is number of vertices
 // Space Complexity: O(V + E)
 bool cycleInGraphHelper(vector<vector<int>> edges, vector<bool> &visited,
-                        int node) {
-  if (visited[node]) {
-    return true;
-  }
+                        vector<bool> &isInCurrentPath, int node) {
   visited[node] = true;
+  isInCurrentPath[node] = true;
   for (int edge = 0; edge < edges[node].size(); edge++) {
-    if (cycleInGraphHelper(edges, visited, edges[node][edge])) {
+    if (!visited[edges[node][edge]]) {
+      if (cycleInGraphHelper(edges, visited, isInCurrentPath,
+                             edges[node][edge])) {
+        return true;
+      }
+    } else if (isInCurrentPath[edges[node][edge]]) {
       return true;
     }
   }
-  visited[node] = false;
+  isInCurrentPath[node] = false;
   return false;
 }
+
 bool cycleInGraph(vector<vector<int>> edges) {
-  vector<bool> visited(edges.size());
+  vector<bool> visited(edges.size(), false);
+  vector<bool> isInCurrentPath(edges.size(), false);
   for (int node = 0; node < edges.size(); node++) {
     if (!visited[node]) {
-      if (cycleInGraphHelper(edges, visited, node)) {
+      if (cycleInGraphHelper(edges, visited, isInCurrentPath, node)) {
         return true;
       }
     }
