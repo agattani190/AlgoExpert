c3a3b3430e60afa848188515743d6cdbe7f8f0f8
Akshay Gattani <agattani@vt.edu>
2021-09-08 17:54:22 -0400

    Min Number of Jumps AlgoExpert

---
+++ AlgoExpert/MinNumberOfJumps.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+int minNumberOfJumps(vector<int> array) {
+  int n = array.size();
+  vector<int> dp(n, INT_MAX);
+  dp[n - 1] = 0;
+  for (int i = n - 2; i >= 0; i--) {
+    for (int j = i + 1; j < n && j <= i + array[i]; j++) {
+      dp[i] = min(dp[j] + 1, dp[i]);
+    }
+  }
+  return dp[0];
+}
