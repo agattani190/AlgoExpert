f1fb67b630088a485bc57db9487877e961fff632
Akshay Gattani <agattani@vt.edu>
2021-09-20 16:52:54 -0400

    Number of ways to traverse graph AlgoExpert

---
+++ AlgoExpert/NumberOfWaysToTraverseGraph.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+// Time Complexity: O(min(height,width))
+// Space Complexity: O(1)
+int numberOfWaysToTraverseGraph(int width, int height) {
+  int a = height + width - 2;
+  int b = min(height, width) - 1;
+  int result = 1;
+  for (int i = 1; i <= b; i++) {
+    result *= (a - i + 1);
+    result /= i;
+  }
+  return result;
+}
