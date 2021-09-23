1a5ef36a0c11a149079900b154b4ce84f1141b98
Akshay Gattani <88683233+agattani190@users.noreply.github.com>
2021-09-23 14:39:03 -0400

    Number Of Ways To Traverse Graph AlgoExpert

--- AlgoExpert/NumberOfWaysToTraverseGraph.cpp
+++ AlgoExpert/NumberOfWaysToTraverseGraph.cpp
@@ -8,8 +8,8 @@ int numberOfWaysToTraverseGraph(int width, int height) {
   int b = min(height, width) - 1;
   int result = 1;
   for (int i = 1; i <= b; i++) {
-    result /= i;
     result *= (a - i + 1);
+    result /= i;
   }
   return result;
 }
