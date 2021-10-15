6d1a472658df2799e578de94e59741866d36d9ad
Akshay Gattani <88683233+agattani190@users.noreply.github.com>
2021-10-15 18:21:30 -0400

    K Closest Points To Origin Using Sorting

--- LeetCode/KClosestPointsToOrigin1.cpp
+++ LeetCode/KClosestPointsToOrigin1.cpp
@@ -6,9 +6,10 @@ static bool compare(vector<int> a, vector<int> b) {
   return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
 }
 
-// Time Complexity: O(mn) -> m, n are the lenth of the two strings
+// Time Complexity: O(nlogn)
 // Space Complexity: O(1)
 vector<vector<int>> kClosest(vector<vector<int>> &points, int k) {
+  int n = points.size();
   sort(points.begin(), points.end(), compare);
   return vector<vector<int>>(points.begin(), points.begin() + k);
 }
